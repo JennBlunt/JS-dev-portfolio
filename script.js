843074f053585d4b56a91f5a00f2dd5f12d830c0
JennBlunt <jennbluntcodes@gmail.com>
2021-10-15 16:39:54 -0400

    Finalizing styling and added email form

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+let factPlaceholder = document.getElementById("jenn-fact");
+let showFact = document.getElementById("show-fact");
+
+let facts = [
+    'Jenn has a strong dislike for the word Penultimate.',
+    'Jenn is allergic to coffee, but not in a can’t-be-in-the-same-room type of allergy.',
+    'Hank made Jenn realize she’s a dog person, not a puppy person.',
+    'In grade 4, Jenn wrote a speech on procrastination, after leaving the assignment to the last minute.',
+    'Jenn loves tea, which is handy given her allergy.'
+]
+let factNumber;
+
+function randomFact() {
+    return facts[factNumber];
+}
+
+showFact.addEventListener('click', function () {
+    factNumber = Math.floor(Math.random() * 5);
+    factPlaceholder.innerHTML = randomFact();
+});
+
+
+
 // const squares = document.querySelectorAll('.square')
 // const mole = document.querySelector('.mole')
 // const timeLeft = document.querySelector('#time')
