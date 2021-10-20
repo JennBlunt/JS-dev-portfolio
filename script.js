a63e7191f537adef26779da4eb6abdcb8147c302
JennBlunt <jennbluntcodes@gmail.com>
2021-10-20 16:16:28 -0400

    Style changes. New fonts, details for projects

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let showFact = document.getElementById("show-fact");
 let facts = [
     'Jenn has a strong dislike for the word Penultimate.',
     'If Jenn had a super power she would want to be fluent in all languages.',
-    'Jenn is not very good a networking, so she made a Resume Tshirt to help.',
+    'Jenn is not very good a networking, so she made a Resume T-shirt to help break the ice.',
     'In grade 4, Jenn wrote a speech on procrastination, after leaving the assignment to the last minute.',
     'Jenn loves tea, which is handy given her allergy.'
 ]
